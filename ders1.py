c32442123de145c6e122b4814d88c76e21c85a15
Umut Karcı <cediddi@gmail.com>
2016-08-07 11:55:00 +0300

    birinci gün.

---
+++ ders1.py
@@ -0,0 +1,35 @@
+otomat = {
+    "çay": 2,
+    "kahve": 3,
+    "çikolata": 5,
+    "oreo": 7,
+    "sigara": 11,
+    "sarı gazoz": 13,
+    "notebook": 17,
+    "eti üçharfliler": 19,
+}
+
+
+def makina(para, gida, adet=1, fail_silently=False):
+    """Bu fonksyon parayı ve gıda türünü (opsiyonel olarak adeti de) alır, duruma göre kalan parayı ve gıdayı döndürür,
+     olmadı ValueError verir, tabi fail_silently derseniz paranızı iade edecektir"""
+    urunler = []
+    kalan = para
+    for i in range(adet):
+        if gida not in otomat:
+            if fail_silently:
+                return para, None
+            raise TypeError("gıda yok")
+        miktar = otomat[gida]
+        if miktar > kalan:
+            if fail_silently:
+                return para, None
+            raise ValueError("para yetmedi")
+        kalan -= miktar
+        urunler.append(gida)
+    if len(urunler) == 1:
+        return kalan, urunler[0]
+    else:
+        return kalan, urunler
+
+kalan_param, gelen_gida = makina(3, "kahve")
