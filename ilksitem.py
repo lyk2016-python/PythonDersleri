a20e661e4c9a0ae9b2f83de965b0fe6170e75554
Umut Karcı <cediddi@gmail.com>
2016-08-12 11:17:34 +0300

    basit bir kullanıcı girişi eklendi,

--- ilksitem.py
+++ ilksitem.py
@@ -1,11 +1,42 @@
 from flask import Flask, render_template
+from flask_httpauth import HTTPBasicAuth
+from hashlib import sha256
 from datetime import datetime
 from random import randint
+from os import path
+import json
 
 app = Flask("ilksitem")
+auth = HTTPBasicAuth()
+
+
+@auth.get_password
+def get_pw(username):
+    if path.exists('db.json'):
+        with open('db.json') as f:
+            try:
+                veriler = json.load(f)
+            except ValueError as e:
+                print("database bozuk: " + e.args)
+                return None
+
+            try:
+                return veriler[username]
+            except KeyError:
+                print("kullanıcı bulunamadı: " + username)
+                return None
+    else:
+        print("database bulunamadı")
+        return None
+
+
+@auth.hash_password
+def hash_pw(password):
+    return sha256(password.encode()).hexdigest()
 
 
 @app.route("/")
+@auth.login_required
 def anasayfa():
     veriler = {
         'zaman': datetime.now(),
@@ -13,7 +44,7 @@ def anasayfa():
     }
 
     for i in range(6):
-        rakam = randint(1,49)
+        rakam = randint(1, 49)
         veriler['kazanan_kupon'].append(rakam)
     return render_template(
             "anasayfa.html", **veriler)
