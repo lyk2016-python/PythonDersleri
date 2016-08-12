867fd99e925e6c3c4e70e51b454a2543ec502efa
Umut Karcı <cediddi@gmail.com>
2016-08-12 12:07:16 +0300

    Not kaydetme özelliği eklendi, flask requestine örnek yazıldı, anasayfa.html'e bir post formu eklendi.

--- ilksitem.py
+++ ilksitem.py
@@ -1,4 +1,11 @@
-from flask import Flask, render_template
+"""
+db.json dosyası içinde bir dict vardır (ya da json object),
+o dict içindeki iki key kullanici ve notlar da birer dict'e key-value bağı ile bağlıdır
+kullanici alt dict'inde key kullanıcı adı, value ise sha256 ile şifrelenmiş parolasıdır
+notlar alt dict'inde key kullanıcı adı, value ise kullanıcının girdiği nottur.
+"""
+
+from flask import Flask, render_template, request
 from flask_httpauth import HTTPBasicAuth
 from hashlib import sha256
 from datetime import datetime
@@ -15,9 +22,12 @@ def get_pw(username):
     if path.exists('db.json'):
         with open('db.json') as f:
             try:
-                veriler = json.load(f)
+                veriler = json.load(f)["kullanici"]
             except ValueError as e:
-                print("database bozuk: " + e.args)
+                print("database bozuk: " + str(e.args))
+                return None
+            except KeyError:
+                print("tablo bulunamadı")
                 return None
 
             try:
@@ -35,12 +45,33 @@ def hash_pw(password):
     return sha256(password.encode()).hexdigest()
 
 
-@app.route("/")
+@app.route("/", methods=['GET', 'POST'])
 @auth.login_required
 def anasayfa():
+    notlar = ""
+    try:
+        with open('db.json') as f:
+            veriler = json.load(f)["notlar"]
+            notlar = veriler[auth.username()]
+    except (FileNotFoundError, KeyError, ValueError) as e:
+        print("HATA: " + str(type(e)) + str(e.args))
+
+    if request.method == "POST" and "notlar" in request.values:
+        try:
+            with open('db.json') as f:
+                veriler = json.load(f)
+                notlar = veriler["notlar"][auth.username()] = request.values["notlar"]
+
+            with open('db.json', "w") as f:
+                json.dump(veriler, f, indent=2)
+
+        except (FileNotFoundError, KeyError, ValueError) as e:
+            print("HATA: " + str(type(e)) + str(e.args))
+
     veriler = {
         'zaman': datetime.now(),
         'kazanan_kupon': [],
+        'notlar': notlar,
     }
 
     for i in range(6):
